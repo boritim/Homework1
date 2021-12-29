9f9a132988602f4ca4973336d94f22feb3da71c8
Borisov Timofey <borisovtim@mail.ru>
2021-12-29 20:25:47 +0300

    [init] Swift Project

---
+++ homework1/homework1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  homework1
+//
+//  Created by Тимофей Борисов on 29.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ homework1/homework1/homework1App.swift
@@ -0,0 +1,17 @@
+//
+//  homework1App.swift
+//  homework1
+//
+//  Created by Тимофей Борисов on 29.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct homework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
